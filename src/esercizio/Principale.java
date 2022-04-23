2c6bb49162a97f90867ea724cb1d64561c9a7165
4b <4b@INF17>
2022-04-23 11:57:07 +0200

    versione 1.00

--- src/esercizio/Principale.java
+++ src/esercizio/Principale.java
@@ -1,5 +1,12 @@
-package esercizio;
-
+/**
+ * @author lorenzo
+ * 
+ * @version 1.00
+ * 
+ */
 public class Principale {
-
+	
+	public static void main(String[]args) {
+		System.out.print("prova github 1 ");
+	}
 }
